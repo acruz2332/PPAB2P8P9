baf47a7d5c53f160c4c0a959706764df7dff125e
acruz2332 <akbar.fajar2301@mail.ugm.ac.id>
2023-05-17 14:50:12 +0700

    Initial commit

---
+++ app/src/main/java/com/ppab/ppab2p8/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ppab.ppab2p8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
